ad6829cda2848b460ffa29801289660f38180f42
Christian Garcia <bones192nd@gmail.com>
2023-08-31 15:34:58 -0400

    ugh

--- server/models.py
+++ server/models.py
@@ -1,14 +1,57 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique = True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', backref='user')
+
+    @hybrid_property
+    def password_hash(self):
+        if not self._password_hash:
+            return self._password_hash
+        raise AttributeError
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f'User {self.username}, ID: {self.id}'    
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    __table_args__ = (
+        db.CheckConstraint('LENGTH(instructions) >= 50'),
+        ) 
+    
+    def __repr__(self):
+        return f'Recipe {self.title}, ID: {self.id}'  
+
+    # @validates('instructions')
+    # def check_instructions(self, key, value):
+    #     if len(value) < 50:
+    #         raise Exception('Your instructions are too short (less than 50 characters).')
+    #     return value
+    
