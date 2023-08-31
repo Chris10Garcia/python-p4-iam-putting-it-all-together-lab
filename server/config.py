00c5a63aeca554ebef6af6197efb855b072af02c
Christian Garcia <bones192nd@gmail.com>
2023-08-31 18:52:46 -0400

    forgot to save all

--- server/config.py
+++ server/config.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
